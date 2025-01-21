1639b0c07267145411717853facf74d7862985bd
llucascr <lucasdecamposranzani@gmail.com>
2025-01-21 16:32:36 -0300

    Projections e GET game by lists

--- src/main/java/com/llucascr/dslist/dto/GameMinDTO.java
+++ src/main/java/com/llucascr/dslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.llucascr.dslist.dto;
 
 import com.llucascr.dslist.entities.Game;
+import com.llucascr.dslist.projections.GameMinProjection;
 
 
 public class GameMinDTO {
@@ -23,7 +24,13 @@ public class GameMinDTO {
         shortDescription = entity.getShortDescription();
     }
 
-
+    public GameMinDTO(GameMinProjection projection) {
+        id = projection.getId();
+        title = projection.getTitle();
+        year = projection.getYear();
+        imgUrl = projection.getImgUrl();
+        shortDescription = projection.getShortDescription();
+    }
 
     // Getters
     public Long getId() {
--- src/main/java/com/llucascr/dslist/services/GameService.java
+++ src/main/java/com/llucascr/dslist/services/GameService.java
@@ -3,6 +3,7 @@ package com.llucascr.dslist.services;
 import com.llucascr.dslist.dto.GameDTO;
 import com.llucascr.dslist.dto.GameMinDTO;
 import com.llucascr.dslist.entities.Game;
+import com.llucascr.dslist.projections.GameMinProjection;
 import com.llucascr.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -27,5 +28,11 @@ public class GameService {
     public GameDTO findById(Long id) {
         Game result = gameRepository.findById(id).get();
         return new GameDTO(result);
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId) {
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
     }
 }
